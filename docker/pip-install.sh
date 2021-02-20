298a8e661bed47f1fbb6d1f1e0e0a2b6a5297356
Andrew A <andrewadev@gmail.com>
2021-02-20 20:28:12 +0100

    Annotate set-up gotcha
    Adding a note for something I encountered during setup.
    
    Note that I had other errors that prevented start-up, not related to the app itself (wrong line endings) that got it into this state

--- docker/pip-install.sh
+++ docker/pip-install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# NOTE: This is using a heuristic (existence of the directory)
+# as a shortcut for validating the presence of the virtual 
+# environment. Certain scenarios (failures during initial setup)
+# can lead to incomplete/invalid virtual envs, where the folders 
+# present, but without useful binaries & packages
+# TODO: Consider more robust validation of virtual env here
 # Create Python virtual environment if not present
 if [ ! -d /opt/openoni/ENV/lib ]; then
   python3 -m venv ENV
 fi
 
 # Activate the Python virtual environment
-source ENV/bin/activate
+source ENV/bin/activate || (echo "Failed to activate virtual environment"; exit -1;)
 
 # Install Open ONI dependencies
 # --no-cache-dir disables building local wheels as packages are installed
